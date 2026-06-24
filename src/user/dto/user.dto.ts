73094a245dc58346fb72bed234bc8a1b983048af
Shivanshu80 <shivamsoni11001@gmail.com>
2026-06-24 21:52:38 +0530

    validation applied

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,5 +1,10 @@
+import { IsString, IsEmail } from 'class-validator'
+
 export class CreateUserDto {
-    name!:string;
-    email!:string;
-    password!:string
+    @IsString()
+    name!: string;
+    @IsEmail()
+    email!: string;
+    @IsString()
+    password!: string;
 }
